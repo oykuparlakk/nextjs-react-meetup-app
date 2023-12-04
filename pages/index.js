827593362b858021dcc40e2bff09ef00d4359d9d
Oyku Parlak <oykuparlak@Oykus-Mac-mini.local>
2023-12-04 04:39:23 -0800

    change layout cumbersome to generic

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,35 @@
-function HomePage(){}
+import MeetupList from "../components/meetups/MeetupList"
+
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        title: 'DENEME',
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/IMG_Academy_Logo.svg/800px-IMG_Academy_Logo.svg.png',
+        address: 'deneme',
+        description: 'dummy data'
+    },
+    {
+        id: 'm2',
+        title: 'DENEME2',
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/IMG_Academy_Logo.svg/800px-IMG_Academy_Logo.svg.png',
+        address: 'deneme',
+        description: 'dummy data'
+    },
+    {
+        id: 'm3',
+        title: 'DENEME3',
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/IMG_Academy_Logo.svg/800px-IMG_Academy_Logo.svg.png',
+        address: 'deneme',
+        description: 'dummy data'
+    }
+];
+
+
+function HomePage() {
+
+    return <MeetupList meetups={DUMMY_MEETUPS} />
+
+
+}
+
+export default HomePage;
