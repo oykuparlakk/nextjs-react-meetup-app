30ca50a7c96dbfef45b4eabac5363809e870f9ec
Oyku Parlak <oykuparlak@Oykus-Mac-mini.local>
2023-12-05 16:56:39 +0300

    fixed

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,13 +1,16 @@
-// /api/new-meetup
 import { MongoClient } from "mongodb";
 
 async function handler(req, res) {
+  let client;
+
+  console.log("asdasdBURADAYIM");
+
   try {
     if (req.method === "POST") {
       const data = req.body;
 
-      const client = await MongoClient.connect(
-        "mongodb+srv://oykuparlak:dOexcWsck4LSSBdM@cluster0.vmxa0mz.mongodb.net/meetups?retryWrites=true&w=majority"
+      client = await MongoClient.connect(
+        "mongodb+srv://oykuparlak:LEHAYzLwCl1TJWot@cluster0.vmxa0mz.mongodb.net/meetups?retryWrites=true&w=majority"
       );
 
       const db = client.db();
@@ -17,15 +20,17 @@ async function handler(req, res) {
 
       console.log(result);
 
-      client.close();
-
       res.status(201).json({ message: "Meetup inserted" });
     } else {
       res.status(405).json({ message: "Method Not Allowed" });
     }
   } catch (error) {
-    console.error('Error in handler:', error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    console.error("Error in handler:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  } finally {
+    if (client) {
+      await client.close();
+    }
   }
 }
 
